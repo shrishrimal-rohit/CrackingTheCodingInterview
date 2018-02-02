9120ef704a111490b8cc0c3a68b09e46b21e0c3d
shrishrimal-rohit <rohit.shrishrimal@sjsu.edu>
2018-02-01 23:05:03 -0800

    intial

--- Arrays&strings/src/OneAway.java
+++ Arrays&strings/src/OneAway.java
@@ -1,2 +1,44 @@
 public class OneAway {
+
+    public static void main(String[] args) {
+
+        System.out.println("Replacement:"+replacement("pale", "bale"));
+        System.out.println("one Edit:"+oneEdit("bole", "bfe"));
+    }
+
+    static boolean replacement(String str1, String str2) {
+        int replace = -1;
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        if (str1.length() != str2.length())
+            return false;
+        for (int i = 0; i < str1.length(); i++) {
+            if (ch1[i] != ch2[i])
+                replace++;
+            if (replace > 0)
+                break;
+        }
+        return replace == 0 ? true : false;
+    }
+
+    static boolean oneEdit(String str1, String str2) {
+        if (str1.length() != (str2.length() + 1)) {
+         System.out.println("inside if");
+            return false;
+        }
+            int index1 = 0;
+        int index2 = 0;
+        while (index1 < str1.length() && index2 < str2.length()) {
+            if (str1.charAt(index1) != str2.charAt(index2)) {
+                if (index1 != index2)
+                    return false;
+                index1++;
+            } else {
+                index1 ++;
+                index2 ++;
+            }
+        }
+
+    return true;
+    }
 }
---
+++ Arrays&strings/src/ZeroMatrix.java
@@ -0,0 +1,46 @@
+public class ZeroMatrix {
+
+    public static void main(String[]args)
+    {
+
+    }
+
+    void setZeros(int[][] matrix) {
+         boolean[] row = new boolean[matrix .length];
+         boolean[] column = new boolean[matrix[0].length];
+
+         // Store the row and column index with value 0
+        for (int i= 0; i < matrix.length; i++) {
+             for (int j = 0; j < matrix[0].length;j++) {
+               if (matrix[i][j] == 0) {
+                  row[i] = true;
+                  column[j] = true;
+               }
+             }
+        }
+
+
+         // Nullify rows
+         for (inti= 0; i < row.length; i++) {
+             if (row[i]) nullifyRow(matrix, i);
+             }
+
+         // Nullify columns
+         for (int j = 0; j < column.length; j++) {
+             if (column[j]) nullifyColumn(matrix, j);
+             }
+         }
+
+         void nullifyRow(int[][] matrix, int row) {
+         for (int j = 0; j < matrix[0].length; j++) {
+             matrix[row][j] = 0;
+             }
+         }
+
+         void nullifyColumn(int[][] matrix, int col) {
+         for (int i= 0; i < matrix.length; i++) {
+             matrix[i][col] = 0;
+             }
+         }
+
+}
